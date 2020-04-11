1a2f8460f6a1ae54a9ec4150583e92a63e7de202
Gavin Kliger <gkliger@berkeley.edu>
2020-04-11 16:26:29 -0700

    Implementing deploy_axis and two axis partition deployments

--- modin/engines/cloudburst/frame/axis_partition.py
+++ modin/engines/cloudburst/frame/axis_partition.py
@@ -1,51 +1,99 @@
 from modin.engines.base.frame.axis_partition import PandasFrameAxisPartition
-from .partition import PandasOnRayFramePartition
+from .partition import PandasOnCloudburstFramePartition
 from modin import __execution_engine__
 
+
+
+# TODO: Move to apply
 if __execution_engine__ == "Cloudburst":
-  # TODO: Determine imports for CB
-  from distributed.client import get_client
-  from distributed import Future
+  from cloudburst.shared.reference import CloudburstReference
+  from modin.engines.cloudburst.utils import get_or_init_client
+
+  cloudburst = get_or_init_client()
 
 
 class PandasOnCloudburstFrameAxisPartition(PandasFrameAxisPartition):
-  def __init__(self, list_of_blocks):
-    # Unwrap from BaseFramePartition object for ease of use
-    for obj in list_of_blocks:
-      obj.drain_call_queue()
-    self.list_of_blocks = [obj.oid for obj in list_of_blocks]
+    def __init__(self, list_of_blocks):
+        # Unwrap from BaseFramePartition object for ease of use
+        for obj in list_of_blocks:
+            obj.drain_call_queue()
+        self.list_of_blocks = [obj.future for obj in list_of_blocks]
 
-  partition_type = PandasOnCloudburstFramePartition
-  if __execution_engine__ == "Cloudburst":
-  # TODO: Check if this is valid
-    instance_type = Future
+    partition_type = PandasOnCloudburstFramePartition
+    if __execution_engine__ == "Cloudburst":
+        # TODO: Determine instance type
+        # DASK: instance_type = Future
+        pass
 
-  @classmethod
-  def deploy_axis_func(
-    cls, axis, func, num_splits, kwargs, maintain_partitioning, *partitions
-  ):
-    # TODO: Implement
+    @classmethod
+    def deploy_axis_func(
+        cls, axis, func, num_splits, kwargs, maintain_partitioning, *partitions
+    ):
+        client = get_or_init_client()
+        r_func = cloudburst.register(
+            PandasFrameAxisPartition.deploy_axis_func, "PandasFrameAxisPartition.deploy_axis_func"
+        )
 
-  @classmethod
-  def deploy_func_between_two_axis_partitions(
-    cls, axis, func, num_splits, len_of_left, kwargs, *partitions
-  ):
-    # TODO: Implement
-
-class PandasOnRayFrameColumnPartition(PandasOnCloudburstFrameAxisPartition):
-  """The column partition implementation for Cloudburst. All of the implementation
-      for this class is in the parent class, and this class defines the axis
-      to perform the computation over.
-  """
-
-  axis = 0
+        axis_result = r_func(
+            axis,
+            func,
+            num_splits,
+            kwargs,
+            maintain_partitioning,
+            *partitions,
+          )
 
 
-class PandasOnRayFrameRowPartition(PandasOnCloudburstFrameAxisPartition):
-  """The row partition implementation for CLoudburst. All of the implementation
-      for this class is in the parent class, and this class defines the axis
-      to perform the computation over.
-  """
+        if num_splits == 1:
+            return axis_result
+        # We have to do this to split it back up. It is already split, but we need to
+        # get futures for each.
+        return [
+            client.submit(lambda l: l[i], axis_result, pure=False)
+            for i in range(num_splits)
+        ]
 
-  axis = 1
+    @classmethod
+    def deploy_func_between_two_axis_partitions(
+        cls, axis, func, num_splits, len_of_left, kwargs, *partitions
+    ):
+        client = get_or_init_client()
+        r_func = cloudburst.register(
+            PandasFrameAxisPartition.deploy_func_between_two_axis_partitions, "PandasFrameAxisPartition.deploy_func_between_two_axis_partitions"
+        )
 
+
+        axis_result = r_func(
+            axis,
+            func,
+            num_splits,
+            len_of_left,
+            kwargs,
+            *partitions,
+        )
+        if num_splits == 1:
+            return axis_result
+        # We have to do this to split it back up. It is already split, but we need to
+        # get futures for each.
+        return [
+            client.submit(lambda l: l[i], axis_result, pure=False)
+            for i in range(num_splits)
+        ]
+
+
+class PandasOnDaskFrameColumnPartition(PandasOnDaskFrameAxisPartition):
+    """The column partition implementation for Multiprocess. All of the implementation
+        for this class is in the parent class, and this class defines the axis
+        to perform the computation over.
+    """
+
+    axis = 0
+
+
+class PandasOnDaskFrameRowPartition(PandasOnDaskFrameAxisPartition):
+    """The row partition implementation for Multiprocess. All of the implementation
+        for this class is in the parent class, and this class defines the axis
+        to perform the computation over.
+    """
+
+    axis = 1
