d7143c86521b8c1e0fc9a9ed3d3f9e98fa86fa37
alclol <alicelyu@berkeley.edu>
2020-04-11 23:29:59 +0000

    scratch

--- modin/engines/cloudburst/frame/axis_partition.py
+++ modin/engines/cloudburst/frame/axis_partition.py
@@ -1,185 +1,76 @@
-from modin.engines.base.frame.partition import BaseFramePartition
-from modin.data_management.utils import length_fn_pandas, width_fn_pandas
-import pandas
+from modin.engines.base.frame.axis_partition import PandasFrameAxisPartition
+from .partition import PandasOnCloudburstFramePartition
 from modin import __execution_engine__
 
-def apply_list_of_funcs(cloudburst, funcs, df):
-    for func, kwargs in funcs:
-        df = func(df, **kwargs)
-    return df
+if __execution_engine__ == "Cloudburst":
+    cloudburst = None
 
-class PandasOnCloudburstFramePartition(BaseFramePartition):
-    def __init__(self, future, length=None, width=None, call_queue=None):
-        self.future = future
-        if call_queue is None:
-            call_queue = []
-        self.call_queue = call_queue
-        self._length_cache = length
-        self._width_cache = width
+class PandasOnCloudburstFrameAxisPartition(PandasFrameAxisPartition):
+    def __init__(self, list_of_blocks):
+        # Unwrap from BaseFramePartition object for ease of use
+        for obj in list_of_blocks:
+            obj.drain_call_queue()
+        self.list_of_blocks = [obj.future for obj in list_of_blocks]
 
-    def get(self):
-        """Flushes the call_queue and returns the data.
+    partition_type = PandasOnCloudburstFramePartition
+#    if __execution_engine__ == "Dask":
+#        instance_type = Future
 
-        Note: Since this object is a simple wrapper, just return the data.
-
-        Returns:
-            The object that was `put`.
-        """
-        self.drain_call_queue()
-        # blocking operation
-        if isinstance(self.future, pandas.DataFrame):
-            return self.future
-        return self.future.get()
-
-    def apply(self, func, **kwargs):
-        """Apply some callable function to the data in this partition.
-
-        Note: It is up to the implementation how kwargs are handled. They are
-            an important part of many implementations. As of right now, they
-            are not serialized.
-
-        Args:
-            func: The lambda to apply (may already be correctly formatted)
-
-        Returns:
-             A new `BaseFramePartition` containing the object that has had `func`
-             applied to it.
-        """
-        call_queue = self.call_queue + [[func, kwargs]]
-
-        if __execution_engine__ == "Cloudburst":
+    @classmethod
+    def deploy_axis_func(
+        cls, axis, func, num_splits, kwargs, maintain_partitioning, *partitions
+    ):
+        if not cloudburst:
             from cloudburst.shared.reference import CloudburstReference
             from modin.engines.cloudburst.utils import get_or_init_client
             cloudburst = get_or_init_client()
 
-        func = cloudburst.register(
-            apply_list_of_funcs, "apply_list_of_funcs"
-        )
-        future = func(call_queue, self.future)
-        return PandasOnCloudburstFramePartition(future)
+        func = PandasFrameAxisPartition.deploy_axis_func
+        # args = [axis, func, num_splits, kwargs, maintain_partitioning, *partitions, pure]
+        args = [axis, func, num_splits, kwargs, maintain_partitioning, *partitions, False]
+        f = cloudburst.register(lambda _, _args: func(*_args), func.__name__)
+        axis_result = f(args)
 
-    def add_to_apply_calls(self, func, **kwargs):
-        return PandasOnCloudburstFramePartition(
-            self.future, call_queue=self.call_queue + [[func, kwargs]]
-        )
+        if num_splits == 1:
+            return axis_result
 
-    def drain_call_queue(self):
-        if len(self.call_queue) == 0:
-            return
-        self.future = self.apply(lambda x: x).future
-        self.call_queue = []
-
-    def mask(self, row_indices, col_indices):
-        new_obj = self.add_to_apply_calls(
-            lambda df: pandas.DataFrame(df.iloc[row_indices, col_indices])
-        )
-        new_obj._length_cache = (
-            len(row_indices)
-            if not isinstance(row_indices, slice)
-            else self._length_cache
-        )
-        new_obj._width_cache = (
-            len(col_indices)
-            if not isinstance(col_indices, slice)
-            else self._width_cache
-        )
-        return new_obj
-
-    def __copy__(self):
-        return PandasOnCloudburstFramePartition(
-            self.future, self._length_cache, self._width_cache
-        )
-
-    def to_pandas(self):
-        """Convert the object stored in this partition to a Pandas DataFrame.
-
-        Note: If the underlying object is a Pandas DataFrame, this will likely
-            only need to call `get`
-
-        Returns:
-            A Pandas DataFrame.
-        """
-        dataframe = self.get()
-        assert type(dataframe) is pandas.DataFrame or type(dataframe) is pandas.Series
-
-        return dataframe
-
-    def to_numpy(self):
-        """Convert the object stored in this parition to a Numpy Array.
-
-        Returns:
-            A Numpy Array.
-        """
-        return self.apply(lambda df: df.to_numpy()).get()
+        unpack = cloudburst.register(lambda _, l, i: l[i], "unpack")
+        return [unpack(axis_result, i) for i in range(num_splits)]
 
     @classmethod
-    def put(cls, obj):
-        """A factory classmethod to format a given object.
+    def deploy_func_between_two_axis_partitions(
+        cls, axis, func, num_splits, len_of_left, kwargs, *partitions
+    ):
+        if not cloudburst:
+            from cloudburst.shared.reference import CloudburstReference
+            from modin.engines.cloudburst.utils import get_or_init_client
+            cloudburst = get_or_init_client()
 
-        Args:
-            obj: An object.
+        func = PandasFrameAxisPartition.deploy_func_between_two_axis_partitions
+        args = [axis, func, num_splits, len_of_left, kwargs, *partitions, False]
+        f = cloudburst.register(lambda _, _args: func(*_args), func.__name__)
+        axis_result = f(args)
 
-        Returns:
-            A `RemotePartitions` object.
-        """
-        import uuid
+        if num_splits == 1:
+            return axis_result
 
-        ref = str(uuid.uuid4())
-        cloudburst.put_object(ref, obj)
-        return cls(DropletReference(ref, deserialize=True))
+        unpack = cloudburst.register(lambda _, l, i: l[i], "unpack")
+        return [unpack(future_obj, i) for i in range(num_splits)]
 
-    @classmethod
-    def preprocess_func(cls, func):
-        """Preprocess a function before an `apply` call.
+class PandasOnCloudburstFrameColumnPartition(PandasOnDaskFrameAxisPartition):
+    """The column partition implementation for Multiprocess. All of the implementation
+        for this class is in the parent class, and this class defines the axis
+        to perform the computation over.
+    """
 
-        Note: This is a classmethod because the definition of how to preprocess
-            should be class-wide. Also, we may want to use this before we
-            deploy a preprocessed function to multiple `BaseFramePartition`
-            objects.
+    axis = 0
 
-        Args:
-            func: The function to preprocess.
 
-        Returns:
-            An object that can be accepted by `apply`.
-        """
-        return func
+class PandasOnCloudburstFrameRowPartition(PandasOnDaskFrameAxisPartition):
+    """The row partition implementation for Multiprocess. All of the implementation
+        for this class is in the parent class, and this class defines the axis
+        to perform the computation over.
+    """
 
-    @classmethod
-    def length_extraction_fn(cls):
-        """The function to compute the length of the object in this partition.
+    axis = 1
 
-        Returns:
-            A callable function.
-        """
-        return length_fn_pandas
-
-    @classmethod
-    def width_extraction_fn(cls):
-        """The function to compute the width of the object in this partition.
-
-        Returns:
-            A callable function.
-        """
-        return width_fn_pandas
-
-    _length_cache = None
-    _width_cache = None
-
-    def length(self):
-        if self._length_cache is None:
-            self._length_cache = self.apply(lambda df: len(df)).future
-        if isinstance(self._length_cache, type(self.future)):
-            self._length_cache = self._length_cache.result()
-        return self._length_cache
-
-    def width(self):
-        if self._width_cache is None:
-            self._width_cache = self.apply(lambda df: len(df.columns)).future
-        if isinstance(self._width_cache, type(self.future)):
-            self._width_cache = self._width_cache.result()
-        return self._width_cache
-
-    @classmethod
-    def empty(cls):
-        return cls(pandas.DataFrame(), 0, 0)
--- modin/engines/cloudburst/task_wrapper.py
+++ modin/engines/cloudburst/task_wrapper.py
@@ -3,10 +3,13 @@ from modin import __execution_engine__
 class CloudburstTask:
     @classmethod
     def deploy(cls, func, num_return_vals, kwargs):
+
         from modin.engines.cloudburst.utils import get_or_init_client
         cloudburst = get_or_init_client()
+
         f = cloudburst.register(lambda _, kwargs: func(**kwargs), func.__name__)
         future_obj = f(kwargs)
+
         unpack = cloudburst.register(lambda _, l, i: l[i], "unpack")
         return [unpack(future_obj, i) for i in range(num_return_vals)]
 
