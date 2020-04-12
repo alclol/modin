776f6febdaf951d8fd6187a1f4139ea12dfa8fdd
Gavin Kliger <gkliger@berkeley.edu>
2020-04-12 12:25:27 -0700

    Skeleton for partition_manager

--- modin/engines/cloudburst/frame/partition_manager.py
+++ modin/engines/cloudburst/frame/partition_manager.py
@@ -1,8 +1,14 @@
 from modin.engines.base.frame.partition_manager import BaseFrameManager
 from modin.engines.cloudburst.frame.partition import PandasOnCloudburstFramePartition
 
+
+from .axis_partition import (
+    PandasOnCloudburstFrameColumnPartition,
+    PandasOnCloudburstFrameRowPartition,
+)
+
 class CloudburstFrameManager(BaseFrameManager):
     # This object uses DropletRemotePartition objects as the underlying store.
     _partition_class = PandasOnCloudburstFramePartition
-    _column_partitions_class = None
-    _row_partition_class = None
+    _column_partitions_class = PandasOnCloudburstFrameColumnPartition
+    _row_partition_class = PandasOnCloudburstFrameRowPartition
