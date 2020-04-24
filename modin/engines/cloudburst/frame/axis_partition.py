3893c52b0b8051ca28f3ec88fe55644e1b793474
Gavin Kliger <gkliger@berkeley.edu>
2020-04-24 13:22:12 -0700

    Wrapping errors

--- modin/engines/cloudburst/frame/axis_partition.py
+++ modin/engines/cloudburst/frame/axis_partition.py
@@ -21,36 +21,45 @@ class PandasOnCloudburstFrameAxisPartition(PandasFrameAxisPartition):
     def deploy_axis_func(
         cls, axis, func, num_splits, kwargs, maintain_partitioning, *partitions
     ):
-        from modin.engines.cloudburst.utils import get_or_init_client
-        cloudburst = get_or_init_client()
+        try:
 
-        args = [axis, func, num_splits, kwargs, maintain_partitioning, *partitions]
-        f = cloudburst.register(lambda _, *args: PandasFrameAxisPartition.deploy_axis_func(*args), "deploy_axis_func")
-        axis_result = f(*args)
+            from modin.engines.cloudburst.utils import get_or_init_client
+            cloudburst = get_or_init_client()
 
-        if num_splits == 1:
-            return axis_result
-        unpack = cloudburst.register(lambda _, l, i: l[i], "unpack")
-        res = [unpack(axis_result, i) for i in range(num_splits)]
-        return res
+            args = [axis, func, num_splits, kwargs, maintain_partitioning, *partitions]
+            f = cloudburst.register(lambda _, *args: PandasFrameAxisPartition.deploy_axis_func(*args), "deploy_axis_func")
+            axis_result = f(*args)
+
+            if num_splits == 1:
+                return axis_result
+            unpack = cloudburst.register(lambda _, l, i: l[i], "unpack")
+            res = [unpack(axis_result, i) for i in range(num_splits)]
+            return res
+        except Exception as e:
+            return e
+
+
 
     @classmethod
     def deploy_func_between_two_axis_partitions(
         cls, axis, func, num_splits, len_of_left, kwargs, *partitions
     ):
-        from modin.engines.cloudburst.utils import get_or_init_client
-        cloudburst = get_or_init_client()
+        try:
+            from modin.engines.cloudburst.utils import get_or_init_client
+            cloudburst = get_or_init_client()
 
-        args = [axis, func, num_splits, len_of_left, kwargs, *partitions]
-        f = cloudburst.register(lambda _, *args: PandasFrameAxisPartition.deploy_func_between_two_axis_partitions(*args), "deploy_between_two_axix_partitions")
+            args = [axis, func, num_splits, len_of_left, kwargs, *partitions]
+            f = cloudburst.register(lambda _, *args: PandasFrameAxisPartition.deploy_func_between_two_axis_partitions(*args), "deploy_between_two_axix_partitions")
 
-        axis_result = f(*args)
+            axis_result = f(*args)
 
-        if num_splits == 1:
-            return axis_result
+            if num_splits == 1:
+                return axis_result
 
-        unpack = cloudburst.register(lambda _, l, i: l[i], "unpack")
-        return [unpack(axis_result, i) for i in range(num_splits)]
+            unpack = cloudburst.register(lambda _, l, i: l[i], "unpack")
+            return [unpack(axis_result, i) for i in range(num_splits)]
+        except Exception as e:
+            return e
 
 class PandasOnCloudburstFrameColumnPartition(PandasOnCloudburstFrameAxisPartition):
     """The column partition implementation for Multiprocess. All of the implementation
