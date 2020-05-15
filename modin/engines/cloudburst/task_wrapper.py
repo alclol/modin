9acaa10f2048ffa4814dd5496e5c3a6e106e5d5a
alclol <alcloll56@gmail.com>
2020-05-15 02:12:10 +0000

    debug prints

--- modin/engines/cloudburst/task_wrapper.py
+++ modin/engines/cloudburst/task_wrapper.py
@@ -12,12 +12,14 @@ class CloudburstTask:
            cloudburst = get_or_init_client()
 
         f = cloudburst.register(lambda _, kwargs: func(**kwargs), func.__name__)
+        print(f"____________task wrapper array size = {num_return_vals}")
         future_obj = f(kwargs)
         unpack = cloudburst.register(lambda _, l, i: l[i], "unpack")
         return [unpack(future_obj, i) for i in range(num_return_vals)]
 
     @classmethod
     def materialize(cls, obj_id):
+        print(f"____________materialize num_obj_id = {len(obj_id)}")
         if isinstance(obj_id, list):
             return [o.get() for o in obj_id]
         return obj_id.get()
